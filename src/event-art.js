14ed4bfa46f026cf79cba3d2074a7c17001d4168
JohnKCabbage <John.C.Cabbage@gmail.com>
2026-08-31 16:25:38 -0400

    Add large Wikimedia Commons event art library

---
+++ src/event-art.js
@@ -0,0 +1,77 @@
+/* Wikimedia Commons visual library for Of All The Russias.
+   Runtime uses Special:Redirect/file so art can be swapped centrally here.
+   Sources/licensing are documented in asset/ASSET_CATALOG.md. */
+
+const OATR_ASSET = {
+  imperialArms: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Lesser%20coat%20of%20arms%20of%20the%20Russian%20Empire.svg',
+  nicholasPhoto: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Portrait%20of%20Czar%20Nicholas%20II.jpg',
+  nicholasRepin: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Nicolas%20II%20of%20Russia%20by%20Iliya%20Repin.jpg',
+  nicholasSerov: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/%D0%92%D0%B0%D0%BB%D0%B5%D0%BD%D1%82%D0%B8%D0%BD%20%D0%90.%20%D0%A1%D0%B5%D1%80%D0%BE%D0%B2%20-%20%D0%9F%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82%20%D0%9D%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D1%8F%20II%20(%D0%A2%D1%80%D0%B5%D1%82%D1%8C%D1%8F%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F%20%D0%B3%D0%B0%D0%BB%D0%B5%D1%80%D0%B5%D1%8F).jpg',
+  alexandra1898: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Russia.%20%D0%9F%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82%20%D0%B8%D0%BC%D0%BF%D0%B5%D1%80%D0%B0%D1%82%D1%80%D0%B8%D1%86%D1%8B%20%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80%D1%8B%20%D0%A4%D1%91%D0%B4%D0%BE%D1%80%D0%BE%D0%B2%D0%BD%D1%8B.%201898%20%D0%AF%D0%B3%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%B8%D0%B9%20v1.jpg',
+  alexandraLoc: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Alexandra%20Fyodorovna%20LOC%2001137u.jpg',
+  witte: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Sergei%20Yulyevich%20Witte%201905.jpeg',
+  wittePortsmouth: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Serge%20Witte%20at%20Portsmouth%20LCCN2005680332.jpg',
+  stolypin: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Stolypin.jpg',
+  stolypinRepin: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Stolypin%20by%20Repin.jpg',
+  kokovtsov: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Vladimir%20Kokovtsov%20portrait%20in%20Niva%20Magazine%201902.jpg',
+  rasputin: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Grigori%20Rasputin%201916.jpg',
+  rasputinFollowers: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Grigory%20Rasputin%20with%20his%20followers.jpg',
+  sazonov: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Sergey%20Sazonov.jpg',
+  grandDukeNicholas: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Grand%20Duke%20Nicholas%20Nikolaevich%20the%20Younger%20(portrait).jpg',
+  sergei: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Grand%20Duke%20Sergei%20Alexandrovich%20of%20Russia%201857-1905.jpg',
+  winterPalace1900: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Winter%20Palace%20in%20St%20petersburg.jpg',
+  winterPalaceDome: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Dome%2C%20Winter%20Palace.jpg',
+  alexanderPalaceHistoric: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Gornostaev%20AM%20Alexander-Palace-in-Tsarskoye-Selo%201847.jpg',
+  alexanderPalaceFacade: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Alexander%20palace.JPG',
+  stateCouncilRepin: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Ilya%20repin%2C%20seduta%20cerimoniale%20del%20consiglio%20di%20stato%20il%207%20maggio%201901%2C%20nel%20centenario%20della%20sua%20fondazione%2C%201903.JPG',
+  bloodySundayMakovsky: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Vladimir%20Makovsky%20%E2%80%94%20The%209th%20of%20January%201905%20on%20Vasilievsky%20Island%20%E2%80%94%20Bloody%20Sunday%20(1905).jpg',
+  bloodySundayKossak: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Bloody%20Sunday%20in%201905%20by%20Wojciech%20Kossak.png',
+  revolutionBarricade: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/The%20Russian%20Revolution%2C%201905%20Q81553.jpg',
+  tsushimaPainting: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Battle%20of%20Tsushima%2C%201905%20by%20T%C5%8Dj%C5%8D%20Sh%C5%8Dtar%C5%8D.jpg',
+  tsushimaWide: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Battle%20of%20Tsushima%20by%20T%C5%8Dj%C5%8D%20Sh%C5%8Dtar%C5%8D.jpg',
+  potemkinPhoto: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Potemkin%20in%20Constantza.JPG',
+  potemkinOdessa: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Potemkin%20mutiny%20Odessa%20le%20kniaz-potemkine%20mouille%20pres%20du%20phare.jpg',
+  potemkinOfficers: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Officers%20and%20petty%20officers%20of%20potemkin.jpg',
+  potemkinFire: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Odessa%20mobs%20burn%20the%20port%20potemkin%20mutiny.jpg',
+  dumaMembers: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Russian%20Duma%20Members%2C%201906.jpg',
+  taurideDuma: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Tauride%20duma.jpg',
+  dumaDiagram: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Russia%20State%20Duma%201906.svg',
+  empireMap: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Russian%20Empire%201914%2C%20modern%20borders.svg',
+  empireFlagMap: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/Flag%20map%20of%20the%20Russian%20Empire%20(1914-1917).png',
+  pantocrator: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/CHRIST_PANTOCRATOR-SINAI(6th_Century).jpg'
+};
+
+const OATR_QUESTION_ART = {
+  1:'bloodySundayMakovsky',2:'bloodySundayKossak',3:'sergei',4:'tsushimaPainting',5:'potemkinPhoto',6:'taurideDuma',7:'wittePortsmouth',8:'revolutionBarricade',9:'witte',10:'bloodySundayMakovsky',11:'potemkinOfficers',12:'revolutionBarricade',13:'stateCouncilRepin',14:'dumaMembers',15:'taurideDuma',16:'stolypin',17:'stolypinRepin',18:'dumaDiagram',
+  19:'taurideDuma',20:'kokovtsov',21:'stolypinRepin',22:'stolypin',23:'empireMap',24:'empireMap',25:'empireMap',26:'grandDukeNicholas',27:'pantocrator',28:'empireMap',29:'empireMap',30:'stateCouncilRepin',31:'dumaMembers',32:'alexandra1898',33:'stolypinRepin',34:'stolypin',35:'stolypinRepin',36:'kokovtsov',37:'empireMap',38:'alexanderPalaceFacade',
+  39:'revolutionBarricade',40:'rasputinFollowers',41:'rasputin',42:'empireMap',43:'grandDukeNicholas',44:'empireMap',45:'nicholasRepin',46:'pantocrator',47:'stateCouncilRepin',48:'empireMap',49:'empireMap',50:'revolutionBarricade',51:'stateCouncilRepin',52:'alexandraLoc',53:'grandDukeNicholas',54:'sazonov',55:'alexanderPalaceHistoric',
+  56:'sazonov',57:'sazonov',58:'empireMap',59:'stateCouncilRepin',60:'stateCouncilRepin',61:'grandDukeNicholas',62:'nicholasSerov',63:'grandDukeNicholas',64:'empireFlagMap',65:'alexandra1898',66:'grandDukeNicholas',67:'sazonov',68:'winterPalace1900',69:'nicholasSerov',70:'pantocrator'
+};
+
+const OATR_ADVISOR_ART = {
+  'GRAND DUKE SERGEI ALEXANDROVICH':'sergei',
+  'COUNT SERGEI WITTE':'witte',
+  'ALEXANDRA FEODOROVNA':'alexandra1898',
+  'PYOTR STOLYPIN':'stolypinRepin',
+  'VLADIMIR KOKOVTSOV':'kokovtsov',
+  'GRIGORI RASPUTIN':'rasputin',
+  'SERGEI SAZONOV':'sazonov',
+  'GRAND DUKE NICHOLAS NIKOLAEVICH':'grandDukeNicholas'
+};
+
+function oatrAssetUrl(key){ return OATR_ASSET[key] || OATR_ASSET.winterPalace1900; }
+function oatrArtForQuestion(id){ return oatrAssetUrl(OATR_QUESTION_ART[id] || 'winterPalace1900'); }
+
+function oatrRefreshArtwork(){
+  const qNumber = Number(document.querySelector('.folio strong')?.textContent || 0);
+  if (qNumber) document.documentElement.style.setProperty('--event-art', `url("${oatrArtForQuestion(qNumber)}")`);
+  const advisorName = document.querySelector('#advisor-name')?.textContent?.trim();
+  if (advisorName && OATR_ADVISOR_ART[advisorName]) {
+    document.documentElement.style.setProperty('--advisor-art', `url("${oatrAssetUrl(OATR_ADVISOR_ART[advisorName])}")`);
+  } else {
+    document.documentElement.style.setProperty('--advisor-art', `url("${oatrAssetUrl('pantocrator')}")`);
+  }
+}
+
+new MutationObserver(oatrRefreshArtwork).observe(document.documentElement,{subtree:true,childList:true});
+window.addEventListener('DOMContentLoaded',oatrRefreshArtwork);
