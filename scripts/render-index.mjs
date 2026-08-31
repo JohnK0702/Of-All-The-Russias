3d6de01fd8b164a56a277760ad261ee16d691a5b
JohnKCabbage <John.C.Cabbage@gmail.com>
2026-08-31 17:12:39 -0400

    Bundle canon Passion aftermath

--- scripts/render-index.mjs
+++ scripts/render-index.mjs
@@ -18,9 +18,10 @@ const detailedAdvisors = await readFile(new URL('src/detailed-advisors.js', root
 const politicalHorror = await readFile(new URL('src/political-horror.js', root), 'utf8');
 const sergeiHorror = await readFile(new URL('src/sergei-horror.js', root), 'utf8');
 const failRoutes = await readFile(new URL('src/fail-routes.js', root), 'utf8');
+const canonPassion = await readFile(new URL('src/canon-passion.js', root), 'utf8');
 const eventArt = await readFile(new URL('src/event-art.js', root), 'utf8');
 const css = `${baseCss}\n\n${darkGuiCss}\n\n${skeletonUiCss}\n\n${sergeiCss}\n\n${assetPlaceholderCss}\n\n${eventArtCss}\n\n${failRoutesCss}`;
-const javascript = `${gameData}\n\n${branching}\n\n${detailedEvents}\n\n${engine}\n\n${endingPaths}\n\n${detailedAdvisors}\n\n${politicalHorror}\n\n${sergeiHorror}\n\n${failRoutes}\n\n${eventArt}`;
+const javascript = `${gameData}\n\n${branching}\n\n${detailedEvents}\n\n${engine}\n\n${endingPaths}\n\n${detailedAdvisors}\n\n${politicalHorror}\n\n${sergeiHorror}\n\n${failRoutes}\n\n${canonPassion}\n\n${eventArt}`;
 
 if (css.includes('</style>') || javascript.includes('</script>')) {
   throw new Error('Inline assets may not contain closing style or script tags.');
