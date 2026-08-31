9eb3896e64e4ec38d7364bdc96a8fdf672d03b88
JohnKCabbage <John.C.Cabbage@gmail.com>
2026-08-31 13:49:01 -0400

    Bundle Sergei assassination interlude

--- scripts/render-index.mjs
+++ scripts/render-index.mjs
@@ -5,11 +5,13 @@ const template = await readFile(new URL('index.template.html', root), 'utf8');
 const baseCss = await readFile(new URL('src/style.css', root), 'utf8');
 const darkGuiCss = await readFile(new URL('src/dark-gui.css', root), 'utf8');
 const skeletonUiCss = await readFile(new URL('src/skeleton-ui.css', root), 'utf8');
+const sergeiCss = await readFile(new URL('src/sergei-horror.css', root), 'utf8');
 const gameData = await readFile(new URL('src/game-data.js', root), 'utf8');
 const branching = await readFile(new URL('src/branching.js', root), 'utf8');
 const engine = await readFile(new URL('src/main.js', root), 'utf8');
-const css = `${baseCss}\n\n${darkGuiCss}\n\n${skeletonUiCss}`;
-const javascript = `${gameData}\n\n${branching}\n\n${engine}`;
+const sergeiHorror = await readFile(new URL('src/sergei-horror.js', root), 'utf8');
+const css = `${baseCss}\n\n${darkGuiCss}\n\n${skeletonUiCss}\n\n${sergeiCss}`;
+const javascript = `${gameData}\n\n${branching}\n\n${engine}\n\n${sergeiHorror}`;
 
 if (css.includes('</style>') || javascript.includes('</script>')) {
   throw new Error('Inline assets may not contain closing style or script tags.');
