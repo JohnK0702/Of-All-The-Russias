435dfb9b527b4a70c5c637ecb3626f12048ca78f
JohnK0702 <John.C.Cabbage@gmail.com>
2026-09-01 13:29:16 -0400

    Bundle legible dossier typography override

--- scripts/render-index.mjs
+++ scripts/render-index.mjs
@@ -12,6 +12,7 @@ const failRoutesCss = await readFile(new URL('src/fail-routes.css', root), 'utf8
 const passionPortraitsCss = await readFile(new URL('src/passion-portraits.css', root), 'utf8');
 const passionUiRevampCss = await readFile(new URL('src/passion-ui-revamp.css', root), 'utf8');
 const marginaliaCss = await readFile(new URL('src/marginalia.css', root), 'utf8');
+const typographyFixCss = await readFile(new URL('src/typography-fix.css', root), 'utf8');
 const gameData = await readFile(new URL('src/game-data.js', root), 'utf8');
 const branching = await readFile(new URL('src/branching.js', root), 'utf8');
 const detailedEvents = await readFile(new URL('src/detailed-events.js', root), 'utf8');
@@ -35,7 +36,7 @@ const marginaliaAssets = await readFile(new URL('src/marginalia-assets.js', root
 // CSS files normally resolve URLs relative to /src. Once inlined into root index.html,
 // those same URLs resolve relative to the page, so normalize repository-local assets here.
 const inlineCss = css => css.replaceAll('../asset/', './asset/');
-const css = `${inlineCss(baseCss)}\n\n${inlineCss(darkGuiCss)}\n\n${inlineCss(skeletonUiCss)}\n\n${inlineCss(sergeiCss)}\n\n${inlineCss(assetPlaceholderCss)}\n\n${inlineCss(eventArtCss)}\n\n${inlineCss(failRoutesCss)}\n\n${inlineCss(passionPortraitsCss)}\n\n${inlineCss(passionUiRevampCss)}\n\n${inlineCss(marginaliaCss)}`;
+const css = `${inlineCss(baseCss)}\n\n${inlineCss(darkGuiCss)}\n\n${inlineCss(skeletonUiCss)}\n\n${inlineCss(sergeiCss)}\n\n${inlineCss(assetPlaceholderCss)}\n\n${inlineCss(eventArtCss)}\n\n${inlineCss(failRoutesCss)}\n\n${inlineCss(passionPortraitsCss)}\n\n${inlineCss(passionUiRevampCss)}\n\n${inlineCss(marginaliaCss)}\n\n${inlineCss(typographyFixCss)}`;
 const javascript = `${gameData}\n\n${branching}\n\n${detailedEvents}\n\n${engine}\n\n${endingPaths}\n\n${balancePass}\n\n${detailedAdvisors}\n\n${politicalHorror}\n\n${sergeiHorror}\n\n${failRoutes}\n\n${canonPassion}\n\n${canonPassionVoice}\n\n${passionUi}\n\n${passionUiRevamp}\n\n${endingEpilogues}\n\n${endingEpiloguesExtra}\n\n${eventArt}\n\n${marginalia}\n\n${marginaliaAssets}`;
 
 if (css.includes('</style>') || javascript.includes('</script>')) {
